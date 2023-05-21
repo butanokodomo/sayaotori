732d95ea1228bcda98dc87a0df21e4b3320d73a1
butanokodomo <t1992dayo@yahoo.co.jp>
2023-05-21 19:23:51 +0900

    realtime and syuuekikeisan checkbocs

--- main.py
+++ main.py
@@ -7,65 +7,60 @@ import datetime as dt
 import matplotlib.dates as mdates
 import pandas_market_calendars as mcal
 #-条件------------------------------------------------------------------------------------------------------------------------------------
-code1 = st.text_input('主')
-code2 = st.text_input('脇')
+st.title('サヤトリ')
+c1, c2 = st.columns([0.2, 0.2]) 
+code1 = c1.text_input('主')
+code2 = c2.text_input('脇')
+c3, c4 = st.columns([0.2, 0.2]) 
+hyouzi = c3.selectbox('グラフの表示日数',(365, 720, 1095))
+kabusa = c4.selectbox('株数誤差％',(5, 10))
+c5 = st.columns([0.2, 0.2]) 
+with c5[0]:syuuekikeisan = st.checkbox('total収益計算')
 
-# 銘柄コード
-#code1 = input('主：')
-#code2 = input('脇：')
-#buyday = input('購入日（例：20220101）：')
-#buyday = dt.datetime.strptime(buyday, '%Y%m%d').date()
-
-
-buyday = '2023-04-11'
 # 株価データの取得①
 #stock1 = yf.Ticker('3289.T')
 #stock2 = yf.Ticker('8804.T')
-stock1 = yf.Ticker(f'{code1}.T')
-stock2 = yf.Ticker(f'{code2}.T')
-
+if code1 and code2:
+    stock1 = yf.Ticker(f'{code1}.T')
+    stock2 = yf.Ticker(f'{code2}.T')
+else:st.warning('主と脇の株価コードを入力してください。')
 # 営業日カレンダーを取得
 jpex = mcal.get_calendar('JPX')
 # 365日前の日付を計算
-one_year_ago = dt.datetime.today() - dt.timedelta(days=960)
-
+one_year_ago = dt.datetime.today() - dt.timedelta(days=hyouzi+120)
 # 365日前から今日までの営業日を取得
 valid_days = jpex.valid_days(start_date=one_year_ago, end_date=dt.datetime.today())
-
 # 最初の営業日を取得
 s = valid_days[0]
 s2 = valid_days[75]
-
 #移動平均日数④
 days = 75
 #-計算------------------------------------------------------------------------------------------------------------------------------------
-
 # DataFrameを作成する
 df1 = stock1.history(start=s)
 df2 = stock2.history(start=s)
 df3 = stock1.history(start=s2)
 df4 = stock2.history(start=s2)
-
 #購入日の始値取得
-open1 = df1.at[str(buyday), 'Open']
-open2 = df2.at[str(buyday), 'Open']
-
-
+open1 = df1['Open'].iloc[-1]
+open2 = df2['Open'].iloc[-1]
 # 直近の営業日 終値取得
 last1 = df1.tail(1)  # データフレームの最後の行を取得
 last2 = df2.tail(1)  # データフレームの最後の行を取得
 close1 = df1['Close'].iloc[-1]
 close2 = df2['Close'].iloc[-1]
-
+#現在価格
+nowprice1 = stock1.info["currentPrice"]
+nowprice2 = stock2.info["currentPrice"]
+current_time = dt.datetime.now().strftime("%H:%M:%S")
 # サヤ比の計算
 saya_ratio = round(df1['Close'] / df2['Close'], 3)
-
+# リアルタイムサヤ比
+newsaya_ratio = round(nowprice1 / nowprice2, 3)
 # 移動平均線の計算
 sma = saya_ratio.rolling(window=days).mean()
-
 # 75日移動平均の計算
 saya_ma = round(saya_ratio.rolling(window=days).mean(), 3)
-
 # ボリンジャーバンドの計算
 std = saya_ratio.rolling(window=days, min_periods=days-1).std()
 upper_band2 = sma + std*2
@@ -89,10 +84,12 @@ correlation = round(df3['Close'].corr(df4['Close']), 3)
 
 # 乖離率の計算
 distance_from_ma =round( (saya_ratio - saya_ma) / saya_ma, 3)
-
+# リアルタイム乖離率
+newdistance_from_ma =round( (newsaya_ratio - saya_ma) / saya_ma, 3)
 # σ値の計算
 sigma_value = round((saya_ratio - saya_ma) / (upper_band2- saya_ma) * 2, 3)
-
+# リアルタイムσ値
+newsigma_value = round((newsaya_ratio - saya_ma) / (upper_band2- saya_ma) * 2, 3)
 #株数算出
 def find_closest_value(row, matrix):
     closest_value = min(matrix, key=lambda x: abs(row - x))
@@ -118,7 +115,7 @@ df = pd.DataFrame(matrix, columns=['株数', 'a', 'b'])
 df['c'] = row4
 df['差率'] = (df['c'] - df['a']) / df['a'] * 100
 
-fi = df[(df['差率'] >= -10) & (df['差率'] <= 10)]#誤差5%以内
+fi = df[(df['差率'] >= -kabusa) & (df['差率'] <= kabusa)]#誤差5%以内
 fir = fi.index[0]+1
 ra = int(df.loc[fir-1, 'c'] / df.loc[0, 'c'] * 100)
 
@@ -126,12 +123,19 @@ ra = int(df.loc[fir-1, 'c'] / df.loc[0, 'c'] * 100)
 kabusu1, kabusu2 = (fir * 100, ra) if open1 == a else (ra, fir * 100)
 
 # 収益を計算
-syueki = (open1 - close1) * kabusu1 + (close2 - open2) * kabusu2 if saya_ratio.mean() > saya_ma.mean() else (open2 - close2) * kabusu2 + (close1 - open1) * kabusu1
+if syuuekikeisan:
+    buy1 = st.text_input('価格1')
+    buy2 = st.text_input('価格2')
+    
+    syueki = (float(buy1) - close1) * kabusu1 + (close2 - float(buy2)) * kabusu2 if saya_ratio.mean() > saya_ma.mean() else (float(buy2) - close2) * kabusu2 + (close1 - float(buy1)) * kabusu1
+    
+    st.write(f'収益：{syueki:,.0f}円')
+
 
 #-印字------------------------------------------------------------------------------------------------------------------------------------
-st.write(f'軸  ：{code1}', f' {kabusu1:,.0f}株')
-st.write(f'脇  ：{code2}', f' {kabusu2:,.0f}株')
-st.write(f'収益：{syueki:,.0f}円')
+st.write(f'1.軸  ：{code1}', f'　{kabusu1:,.0f}株',f'　 {nowprice1:,.0f}円')
+st.write(f'2.脇  ：{code2}', f'　 {kabusu2:,.0f}株'f'　{nowprice2:,.0f}円')
+
 table = pd.DataFrame({
     "相関関数": correlation,
     "σ値": sigma_value,
@@ -141,10 +145,21 @@ table = pd.DataFrame({
     "交差回数": crossover_count,
     
 })
-
+# リアルタイム収益を計算
+newsyueki = (close1 - nowprice1) * kabusu1 + (nowprice2 - close2) * kabusu2 if saya_ratio.mean() > saya_ma.mean() else (close2 - nowprice2) * kabusu2 + (nowprice1 - close1) * kabusu1
+table1 = pd.DataFrame({
+    
+    "収益": f'{newsyueki:,.0f}',
+    "σ値": newsigma_value,
+    "サヤ比": newsaya_ratio,
+    "乖離率": (newdistance_from_ma * 100).round(3).astype(str) + "%",
+})
 # テーブルの表示
 #table['行数'] = range(1, len(table) + 1)　行数追加でチェックする場合用
-st.write(table.tail(5))  #表示する行数を入れる
+st.write(table.tail(5)) #表示する行数を入れる
+#リアルタイムテーブル
+st.write("時間：", current_time)
+st.write(table1.tail(1))
 
 import streamlit as st
 
@@ -162,4 +177,5 @@ ax.set_xlim(saya_ratio.index[75], saya_ratio.index[-1])  # 75日目から表示
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d'))
 
 # Streamlitでグラフを表示
+ # Streamlitでグラフを表示
 st.pyplot(fig)
